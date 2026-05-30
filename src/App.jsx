5d3c5e170705bdcd6bf23572474ba2ce5cb0d8a0
ur1el0 <zanoroosc@gmail.com>
2026-05-30 23:58:44 +0800

    create: about section drafting

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import "./App.css";
 import { Navbar } from "./components/layout/Navbar";
 import { HeroSection } from "./sections/HeroSection";
 import { ProjectSection } from "./sections/ProjectSection";
+import { AboutSection } from "./sections/AboutSection";
 
 function App() {
     return (
-        <>
+        <div className="pt-14">
             <Navbar />
             <HeroSection />
             <ProjectSection />
-        </>
+            <AboutSection />
+        </div>
     );
 }
 
---
+++ src/sections/AboutSection.jsx
@@ -0,0 +1,18 @@
+
+export const AboutSection = () => {
+    return (
+        <section className="py-16 px-4 max-w-4xl mx-auto" id="about">
+            <h2 className="text-3xl font-semibold mb-6 text-center dark:text-white-500">
+                About Me
+            </h2>
+            <div className="text-left max-w-3xl mx-auto text-lg text-gray-700 dark:text-gray-500 ">
+                <p className="text-lg text-gray-700 dark:text-gray-500 mb-4">
+                    I'm Roosc Zaño, a full-stack web and mobile developer with a passion for creating intuitive and efficient applications.    
+                </p>
+                <p className="text-lg text-gray-700 dark:text-gray-500 mb-4">
+                    With a background in Information Technology, I have experience in various programming languages and frameworks, including Node.js, ASP.NET Core, React, and Django. I enjoy working on projects that solve real-world problems and enhance user experiences.
+                </p>
+            </div>
+        </section>
+    );
+}
