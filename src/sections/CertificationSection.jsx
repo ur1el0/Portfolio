0bbf7b6ad771c481b1a30a84a42b52bc0e3c8541
ur1el0 <zanoroosc@gmail.com>
2026-06-02 12:19:36 +0800

    add: certifications

---
+++ src/sections/CertificationSection.jsx
@@ -0,0 +1,20 @@
+
+export const CertificationSection = () => 
+{
+    return (
+        <section className="py-16 text-lg font-bold uppercase tracking-[0.25em] text-[var(--muted)] bg-[var(--bg)] border-t border-[var(--border)] text-center pl-16" id="certifications">
+            <div className="container">
+                <div className="space-y-10">
+                    <div>
+                        <p className="text-lg font-bold uppercase tracking-[0.25em] text-[var(--muted)] ">
+                            Certifications
+                        </p>
+                        <h3 className="mt-2 text-2xl font-semibold text-[var(--text)]">
+                            My Achievements
+                        </h3>
+                    </div>  
+                </div>
+            </div>
+        </section>
+    )
+}
